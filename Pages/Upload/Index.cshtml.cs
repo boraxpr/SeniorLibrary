45f288206b92c36d0f7879a0fc8cd975799ecaeb
boraxpr <naipawat.poo@student.mahidol.ac.th>
2021-05-23 18:11:16 +0700

    Refined Pages/Requests View for Lecturer There was a problem when string BookAdvisor and string Lecturer's Username having a different case causing Lecturers unable to see some Book Requests. Now, Lecturer's Username and BookAdvisor are forced lowercase before checking "string.Contains()".

--- Pages/Upload/Index.cshtml.cs
+++ Pages/Upload/Index.cshtml.cs
@@ -8,6 +8,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using SeniorLibrary.Models;
+using System.Globalization;
 
 namespace SeniorLibrary.Pages.Upload
 {
